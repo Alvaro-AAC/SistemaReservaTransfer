a5147439de35b615df3738f31b6291af0ebdc2c1
Alvaro-AAC <alvaro.arenas973@gmail.com>
2022-07-08 18:32:16 -0400

    Front-end pagina inicial

--- proyecto/models.py
+++ proyecto/models.py
@@ -1,16 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class Chofer(models.Model):
-    nomchofer = models.CharField(max_length=100, verbose_name='Nombre del chofer')
-    tipovehiculo = models.CharField(max_length=50, verbose_name='Tipo de vehiculo')
-    empresa = models.CharField(max_length=100, verbose_name='Nombre de la empresa')
-
-
-    def __str__(self):
-        return self.nomchofer
-
-
-class Vehiculo(models.Model):
-    colorvehiculo = models.CharField(max_length=25, verbose_name='Color del vehiculo')
--- proyecto/views.py
+++ proyecto/views.py
@@ -2,10 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home (request):
-
+def home(request):
     return render(request, 'core/home.html')
-
-def datos (request):
-
-    return render(request, 'core/datos.html')
